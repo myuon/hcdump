a68d3e0280dc0c47a427df313f1f0f6c57954136
myuon <ioi.joi.koi.loi@gmail.com>
2019-05-03 18:41:19 +0900

    feat: cli

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,29 @@
 module Main where
 
+import qualified Data.ByteString as B
+import Options.Applicative
+import qualified GHC
+import qualified GHC.Paths
+import qualified Outputable
+
+data Arg = Arg {
+  filepath :: FilePath
+} deriving (Eq, Show)
+
+argParser :: Parser Arg
+argParser =
+  Arg <$> strOption (long "input" <> short 'i' <> help "Path to the source")
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runCLI =<< execParser opts
+ where
+  opts = info
+    (argParser <**> helper)
+    ( fullDesc <> progDesc "Analyze the dumped GHC Core program." <> header
+      "core-dump (ddc) - GHC Core analyzer"
+    )
+
+runCLI :: Arg -> IO ()
+runCLI arg = do
+  src <- B.readFile (filepath arg)
+  B.putStrLn src
